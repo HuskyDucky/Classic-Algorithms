ee21ec023d783cd93823c810982de863062ca4d4
TheArquitect <44051807+TheArquitect@users.noreply.github.com>
2019-03-05 18:36:37 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -5,11 +5,13 @@
 
 void Binary_Tree_Search();
 void Dynamic_Programming();
+void Greedy();
 
 int main() {
 
     Binary_Tree_Search();
     Dynamic_Programming();
+    Greedy();
 
     return 0;
 }
