4326ec01bc776d0b140927bb8d246ac293b11f70
TheArquitect <44051807+TheArquitect@users.noreply.github.com>
2019-03-08 14:00:08 +0200

    Add files via upload

--- _lib_/_Lib_Edge.h
+++ _lib_/_Lib_Edge.h
@@ -1,8 +1,15 @@
 /**
     File    : _Lib_Edge.h
     Author  : Menashe Rosemberg
+    Created : 2019.03.02            Version: 20190307.2
 
-    Edge
+    Edge's variation type
+
+    Menashe Rosemberg   Israel +972-52-323-0538
+    Copyright(c) 2019      All rights reserved.
+
+    Software distributed under the License is distributed on an "AS IS" BASIS,
+    NO WARRANTIES OR CONDITIONS OF ANY KIND, explicit or implicit.
 **/
 #ifndef EDGE_H
 #define EDGE_H
--- _lib_/_Lib_QuantVertices.cpp
+++ _lib_/_Lib_QuantVertices.cpp
@@ -1,8 +1,15 @@
 /**
     File    : _Lib_QuantVertices.cpp
     Author  : Menashe Rosemberg
+    Created : 2019.03.03            Version: 20190303.2
 
-   Return the quantity of vertices from a vector of edges
+    Return the quantity of vertices from a vector of edges
+
+    Menashe Rosemberg   Israel +972-52-323-0538
+    Copyright(c) 2019      All rights reserved.
+
+    Software distributed under the License is distributed on an "AS IS" BASIS,
+    NO WARRANTIES OR CONDITIONS OF ANY KIND, explicit or implicit.
 **/
 #include <set>
 #include <vector>
@@ -14,7 +21,7 @@ using namespace std;
 template <typename EDGE>
 VertexType TotOfVertices(const vector<EDGE>& edges) {
     try {
-        set<VertexType> VertexList;
+        set<VertexType> VertexList;                     //the edges elements are the vertices itself as well
 
         for (auto& edge : edges) {
             VertexList.emplace(edge.Vertex_1());
