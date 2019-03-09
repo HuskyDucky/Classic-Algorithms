401902e22c08b104f39d03d96492fd57f420eb72
TheArquitect <44051807+TheArquitect@users.noreply.github.com>
2019-03-09 16:22:55 +0200

    Add files via upload

--- Dynamic_Programming/Knapsack.h
+++ Dynamic_Programming/Knapsack.h
@@ -3,6 +3,12 @@
     Author  : Menashe Rosemberg
 
     Dynamic Programming - Knapsack 0/1
+
+    Menashe Rosemberg   Israel +972-52-323-0538
+    Copyright(c) 2019      All rights reserved.
+
+    Software distributed under the MIT License is distributed on an "AS IS" BASIS,
+    NO WARRANTIES OR CONDITIONS OF ANY KIND, explicit or implicit.
 **/
 #ifndef KNAPSACK_H
 #define KNAPSACK_H
