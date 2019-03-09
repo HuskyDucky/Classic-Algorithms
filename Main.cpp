9c8bf332fe8e356127a9d641bb41ce1faf0fce0b
TheArquitect <44051807+TheArquitect@users.noreply.github.com>
2019-03-09 16:20:36 +0200

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -8,68 +8,46 @@
     Menashe Rosemberg   Israel +972-52-323-0538
     Copyright(c) 2019      All rights reserved.
 
-    Software distributed under the License is distributed on an "AS IS" BASIS,
+    Software distributed under the MIT License is distributed on an "AS IS" BASIS,
     NO WARRANTIES OR CONDITIONS OF ANY KIND, explicit or implicit.
 **/
 
 #include <vector>
-#include "_lib_/Ask.h"
+#include "_lib_/Ask_Menu.h"
 
 void Run_Cycle_Detection_in_Linked_List();
 void Run_Cycle_Detection_in_Linked_List_Brent();
-void Run_BinaryTreeSearch_DFS_Interactive();
 void Run_BinaryTree_Compression_Huffman();
 void Run_BinaryTreeSearch_BFS();
+void Run_BinaryTreeSearch_DFS();
+void Run_BinaryTreeSearch_DFS_Strong_Connected_Tarjan();
+void Run_Greedy_Kruskal();
+void Run_Greedy_Dijkastra();
+void Run_Graph_Floyd();
 void Run_Dynamic_Programming_Kadane();
 void Run_Dynamic_Programming_Knapsack_0_1();
 void Run_Dynamic_Programming_LCS();
 void Run_Dynamic_Programming_LPS();
-void Run_Greedy_Kruskal();
-void Run_Greedy_Dijkastra();
-void Run_Graph_Floyd();
 
 int main() {
 
-    std::vector<std::string> Options {
-                                "Linked List - Cycle_Detection",
-                                "Linked List - Cycle_Detection - Brent",
-                                "Binary Tree - Compression - Huffman",
-                                "Binary Tree Search - DFS Interactive",
-                                "Binary Tree Search - BFS",
-                                "Dynamic Programming- Kadane",
-                                "Dynamic Programming- Knapsack 0 1",
-                                "Dynamic Programming- LCS",
-                                "Dynamic Programming- LPS",
-                                "Graph - Minimum Spanning Tree - Floyd Warshall",
-                                "Graph - Greedy - Minimum Spanning Tree - Kruskal",
-                                "Graph - Greedy - Minimum Spanning Tree - Dijkastra",
-                                "Exit"
+    ListOfOptQuest Options {{"Linked List - Cycle_Detection",                       [](){ Run_Cycle_Detection_in_Linked_List();                 }},
+                            {"Linked List - Cycle_Detection - Brent",               [](){ Run_Cycle_Detection_in_Linked_List_Brent();           }},
+                            {"Binary Tree - Compression - Huffman",                 [](){ Run_BinaryTree_Compression_Huffman();                 }},
+                            {"Binary Tree Search - BFS",                            [](){ Run_BinaryTreeSearch_BFS();                           }},
+                            {"Binary Tree Search - DFS Interactive",                [](){ Run_BinaryTreeSearch_DFS();                           }},
+                            {"Binary Tree Search - DFS - Strong Connected - Tarjan",[](){ Run_BinaryTreeSearch_DFS_Strong_Connected_Tarjan();   }},
+                            {"Graph - Minimum Spanning Tree - Floyd Warshall",      [](){ Run_Graph_Floyd();                                    }},
+                            {"Graph - Greedy - Minimum Spanning Tree - Kruskal",    [](){ Run_Greedy_Kruskal();                                 }},
+                            {"Graph - Greedy - Minimum Spanning Tree - Dijkastra",  [](){ Run_Greedy_Dijkastra();                               }},
+                            {"Dynamic Programming- Kadane",                         [](){ Run_Dynamic_Programming_Kadane();                     }},
+                            {"Dynamic Programming- Knapsack 0 1",                   [](){ Run_Dynamic_Programming_Knapsack_0_1();               }},
+                            {"Dynamic Programming- LCS",                            [](){ Run_Dynamic_Programming_LCS();                        }},
+                            {"Dynamic Programming- LPS",                            [](){ Run_Dynamic_Programming_LPS();                        }}
     };
 
-    Ask TheQuestion;
-    TheQuestion << "\n\nChoose the Algorithm you are leaning:\n";
-    TheQuestion.hastheAnswers(move(Options));
-    TheQuestion.hastheBehavior(ASK::ANSWARE_BY_LETTERS);
-
-    for(;;) {
-        char Answer = TheQuestion.wasmade();
-
-        switch (Answer) {
-            case 'a': Run_Cycle_Detection_in_Linked_List();         continue;
-            case 'b': Run_Cycle_Detection_in_Linked_List_Brent();   continue;
-            case 'c': Run_BinaryTree_Compression_Huffman();         continue;
-            case 'd': Run_BinaryTreeSearch_DFS_Interactive();       continue;
-            case 'e': Run_BinaryTreeSearch_BFS();                   continue;
-            case 'f': Run_Dynamic_Programming_Kadane();             continue;
-            case 'g': Run_Dynamic_Programming_Knapsack_0_1();       continue;
-            case 'h': Run_Dynamic_Programming_LCS();                continue;
-            case 'i': Run_Dynamic_Programming_LPS();                continue;
-            case 'j': Run_Graph_Floyd();                            continue;
-            case 'k': Run_Greedy_Kruskal();                         continue;
-            case 'l': Run_Greedy_Dijkastra();                       continue;
-        }
-        break;
-    }
+    Ask_Menu TheQuestion("\n\nChoose the Algorithm you are leaning:\n", move(Options));
+    TheQuestion.Show();
 
     return 0;
 }
